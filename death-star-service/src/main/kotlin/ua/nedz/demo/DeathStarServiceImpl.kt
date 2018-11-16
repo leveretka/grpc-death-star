80b5cda8b18c603926b38ee1c8acaf245d2e858d
marharytan <marharytan@wix.com>
2018-11-16 02:40:10 +0200

    Planet Service used in Death star

--- death-star-service/src/main/kotlin/ua/nedz/demo/DeathStarServiceImpl.kt
+++ death-star-service/src/main/kotlin/ua/nedz/demo/DeathStarServiceImpl.kt
@@ -1,26 +1,47 @@
 package ua.nedz.demo
 
+import com.google.protobuf.Empty
+import io.grpc.ManagedChannelBuilder
+import io.grpc.internal.DnsNameResolverProvider
+import io.grpc.util.RoundRobinLoadBalancerFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
-import ua.nedz.grpc.DeathStarProto
-import ua.nedz.grpc.DeathStarServiceGrpcKt
-import ua.nedz.grpc.PlanetProto
+import ua.nedz.grpc.*
 
 class DeathStarServiceImpl : DeathStarServiceGrpcKt.DeathStarServiceImplBase() {
 
+    private var planetTarget: String? = System.getenv("PLANET_SERVICE_TARGET")
+
+    init {
+        if (planetTarget.isNullOrEmpty()) {
+            planetTarget = "192.168.0.102:50061"
+        }
+    }
+
+    private val planetChannel = ManagedChannelBuilder
+            .forTarget(planetTarget)
+            .nameResolverFactory(DnsNameResolverProvider())
+            .loadBalancerFactory(RoundRobinLoadBalancerFactory.getInstance())
+            .usePlaintext(true)
+            .build()
+
+    private val planetStub = PlanetServiceGrpcKt.newStub(planetChannel)
+
+
     @ExperimentalCoroutinesApi
     override suspend fun destroy(requests: ReceiveChannel<DeathStarProto.DestroyPlanetRequest>) = produce<PlanetProto.Planets> {
-        //getAllPlanets
-        val planetsList = listOf<PlanetProto.Planet>()
-        send(PlanetProto.Planets.newBuilder().addAllPlanets(planetsList).build())
+        send(planetStub.getAllPlanets(Empty.getDefaultInstance()))
             for (request in requests) {
-                //remove planet
+                planetStub.removePlanet(PlaneServiceProto.RemovePlanetRequest
+                        .newBuilder()
+                        .setPlanetId(request.planetId)
+                        .build())
                 //write score
                 //send event
-                //generate new planet
-                //get all planets
-                send(PlanetProto.Planets.newBuilder().addAllPlanets(planetsList).build())
+                planetStub.generateNewPlanet(Empty.getDefaultInstance())
+                send(planetStub.getAllPlanets(Empty.getDefaultInstance())
+                )
             }
     }
 
